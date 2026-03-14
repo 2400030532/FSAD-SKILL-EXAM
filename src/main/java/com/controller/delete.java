499fc4c816696892c0ce15b59869ea84645a6534
LANKA NAGAMOHAN <2400030532@kluniversity.in>
2026-03-14 19:42:57 +0530

    Add DeleteEmployee class for deleting an employee

---
+++ src/main/java/com/controller/delete.java
@@ -0,0 +1,30 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteEmployee {
+
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        cfg.addAnnotatedClass(Employee.class);
+
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        Employee emp = session.get(Employee.class,1);
+
+        if(emp!=null){
+            session.delete(emp);
+        }
+
+        tx.commit();
+
+        session.close();
+        factory.close();
+    }
+}
